e68ceba40c7545da99a69e446188d84b7fff1de5
gustcar <34460151+gustcar@users.noreply.github.com>
2026-07-17 16:14:24 +0000

    feat(tb): integrated env into base_test build_phase

--- tb/tests/base_test.h
+++ tb/tests/base_test.h
@@ -6,17 +6,26 @@
 #define BASE_TEST_H
 
 #include <uvm>
+#include "../env/env.h"
 
 class base_test : public uvm::uvm_test {
 public:
     UVM_COMPONENT_UTILS(base_test);
 
-    explicit base_test(const char* name = "base_test")
-        : uvm::uvm_test(name) {}
+    env* m_env;
 
-    void run_phase(uvm::uvm_phase& phase) override {
+    explicit base_test(const char* name = "base_test")
+        : uvm::uvm_test(name), m_env(nullptr) {}
+
+    virtual void build_phase(uvm::uvm_phase& phase) override {
+        uvm::uvm_test::build_phase(phase);
+        m_env = env::type_id::create("m_env", this);
+        UVM_INFO("BASE_TEST", "Environment created", uvm::UVM_NONE);
+    }
+
+    virtual void run_phase(uvm::uvm_phase& phase) override {
         phase.raise_objection(this);
-        uvm::uvm_report_info("BASE_TEST", "Test running...");
+        UVM_INFO("BASE_TEST", "Test + env running", uvm::UVM_NONE);
         sc_core::wait(10, sc_core::SC_NS);
         phase.drop_objection(this);
     }
